03f2b1b8c3a8249db5818bbfc38c27029684ba69
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-04 16:48:14 +0800

    Part 1 Section 5: Added sample variables with different data types

--- Section1/hello/src/main.rs
+++ Section1/hello/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
+
+    let integer = 2;
+    let string = "hello";
+    let character = 'j';
+    let float = 0.5;
+    let mut mutable_strong = "Bill";
+    let boolean = false;
+    let variable = float;
+
     println!("Hello, world!");
 }
