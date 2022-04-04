329e8d4375d9755e4385ad123297bf02568288ec
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-04 16:14:43 +0800

    Section 1 Part 1: Installed Rust, and tested the main.rs file

---
+++ Section1/hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\Kenywil-Lenovo\Documents\GitRepos\rust-snippets-demo\Section1\hello\target\debug\deps\hello.exe: src\main.rs
+
+C:\Users\Kenywil-Lenovo\Documents\GitRepos\rust-snippets-demo\Section1\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
---
+++ Section1/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\Kenywil-Lenovo\Documents\GitRepos\rust-snippets-demo\Section1\hello\target\debug\hello.exe: C:\Users\Kenywil-Lenovo\Documents\GitRepos\rust-snippets-demo\Section1\hello\src\main.rs
