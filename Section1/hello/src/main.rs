e82c7744744b2ecf3d67f778730811b88df0d846
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-04 17:00:11 +0800

    Section 1 Part 6: Added a sample add function, and variables that call the function

--- Section1/hello/src/main.rs
+++ Section1/hello/src/main.rs
@@ -4,9 +4,19 @@ fn main() {
     let string = "hello";
     let character = 'j';
     let float = 0.5;
-    let mut mutable_strong = "Bill";
+    let mut mutable_string = "Bill";
     let boolean = false;
     let variable = float;
 
+    let add_x = add (1, 1);
+    let add_y = add (3, 0);
+    let add_z = add (add_x, 1);
+
     println!("Hello, world!");
+
 }
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
