7c55a404be4964bf1133af73be5ba58855835ef1
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-05 22:42:51 +0800

    Section 2 Part 15: Added personal solution to activity a3b

--- Section2/section2_activities/src/main.rs
+++ Section2/section2_activities/src/main.rs
@@ -1,6 +1,9 @@
 mod a3a;
+mod a3b;
 
 fn main() {
     // call function from a3a.rs
     a3a::displayGreeting(true);
+    // call function from a3b.rs
+    a3b::dsiplayAmount(1);
 }
--- Section2/section2_activities/target/debug/deps/section2_activities.d
+++ Section2/section2_activities/target/debug/deps/section2_activities.d
@@ -1,6 +1,7 @@
-C:\Users\Kenywil-Lenovo\Documents\GitRepos\rust-snippets-demo\Section2\section2_activities\target\debug\deps\section2_activities.exe: src\main.rs src\a3a.rs
+C:\Users\Kenywil-Lenovo\Documents\GitRepos\rust-snippets-demo\Section2\section2_activities\target\debug\deps\section2_activities.exe: src\main.rs src\a3a.rs src\a3b.rs
 
-C:\Users\Kenywil-Lenovo\Documents\GitRepos\rust-snippets-demo\Section2\section2_activities\target\debug\deps\section2_activities.d: src\main.rs src\a3a.rs
+C:\Users\Kenywil-Lenovo\Documents\GitRepos\rust-snippets-demo\Section2\section2_activities\target\debug\deps\section2_activities.d: src\main.rs src\a3a.rs src\a3b.rs
 
 src\main.rs:
 src\a3a.rs:
+src\a3b.rs:
