239cfa358373cc0555cd6bbba9616308b600bd20
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-05 11:16:22 +0800

    Section 1 Part 7: Added println macros

--- Section1/hello/src/main.rs
+++ Section1/hello/src/main.rs
@@ -8,9 +8,13 @@ fn main() {
     let boolean = false;
     let variable = float;
 
+    println!("Variables: {:?} {:?} {:?} {:?} {:?} {:?} {:?}", integer, string, character, float, mutable_string, boolean, variable);
+
     let add_x = add (1, 1);
     let add_y = add (3, 0);
     let add_z = add (add_x, 1);
+
+    println!("Function calls: {:?} {:?} {:?}", add_x, add_y, add_z);
 
     println!("Hello, world!");
 
