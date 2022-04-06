b06e63fd0feeaba5d967b89c1c4ceb199bf558a3
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-06 12:45:03 +0800

    Section 2 Part 18: Added personal solution to exercise a4a

--- Section2/section2_activities/src/main.rs
+++ Section2/section2_activities/src/main.rs
@@ -1,9 +1,12 @@
 mod a3a;
 mod a3b;
+mod a4a;
 
 fn main() {
-    // call function from a3a.rs
+    // call function from a3a.rs, takes argument boolean
     a3a::displayGreeting(true);
-    // call function from a3b.rs
+    // call function from a3b.rs, takes argument i32
     a3b::dsiplayAmount(1);
+    // call function from a4a.rs, takes argument boolean
+    a4a::displayBoolean(true);
 }
--- Section2/section2_activities/target/debug/deps/section2_activities.d
+++ Section2/section2_activities/target/debug/deps/section2_activities.d
@@ -1,7 +1,8 @@
-C:\Users\Kenywil-Lenovo\Documents\GitRepos\rust-snippets-demo\Section2\section2_activities\target\debug\deps\section2_activities.exe: src\main.rs src\a3a.rs src\a3b.rs
+C:\Users\Kenywil-Lenovo\Documents\GitRepos\rust-snippets-demo\Section2\section2_activities\target\debug\deps\section2_activities.exe: src\main.rs src\a3a.rs src\a3b.rs src\a4a.rs
 
-C:\Users\Kenywil-Lenovo\Documents\GitRepos\rust-snippets-demo\Section2\section2_activities\target\debug\deps\section2_activities.d: src\main.rs src\a3a.rs src\a3b.rs
+C:\Users\Kenywil-Lenovo\Documents\GitRepos\rust-snippets-demo\Section2\section2_activities\target\debug\deps\section2_activities.d: src\main.rs src\a3a.rs src\a3b.rs src\a4a.rs
 
 src\main.rs:
 src\a3a.rs:
 src\a3b.rs:
+src\a4a.rs:
