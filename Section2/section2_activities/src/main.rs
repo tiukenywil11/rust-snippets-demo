e5f76ab692b3762a8e2b215c331dd9d6b417d7cd
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-06 12:52:18 +0800

    Section 2 Part 19: Added personal solution to exercise a4b

--- Section2/section2_activities/src/main.rs
+++ Section2/section2_activities/src/main.rs
@@ -1,6 +1,7 @@
 mod a3a;
 mod a3b;
 mod a4a;
+mod a4b;
 
 fn main() {
     // call function from a3a.rs, takes argument boolean
@@ -9,4 +10,6 @@ fn main() {
     a3b::dsiplayAmount(1);
     // call function from a4a.rs, takes argument boolean
     a4a::displayBoolean(true);
+    // call function from a4a.rs, takes argument i32
+    a4b::displayNumber(1);
 }
