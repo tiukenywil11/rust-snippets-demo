a6a4c1b54ee7c39007bf90628a780ee204252ec0
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-06 12:39:40 +0800

    Section 2 Part 17: Added more match statement examples

--- Section2/conditionals/src/main.rs
+++ Section2/conditionals/src/main.rs
@@ -16,4 +16,13 @@ fn main() {
         _ => println!("its something else"),
     }
 
+    let my_name = "Jayson";
+
+    match my_name {
+        "Jayson" => println!("that is my name"),
+        "Bob" => println!("not my name"),
+        "Alice" => println!("hello alice"),
+        _ => println!("nice to meet you!"),
+    }
+
 }
