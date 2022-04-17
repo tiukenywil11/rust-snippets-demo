b377b031337e981ef408382994cd2ef177119f2d
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-17 12:58:08 +0800

    Section 6 Part 46: Added more examples of String and borrowed string

--- Section6/data-collection/src/main.rs
+++ Section6/data-collection/src/main.rs
@@ -13,6 +13,15 @@ struct Employee {
     name: String,
 }
 
+struct LineItem {
+    name: String,
+    count: i32,
+}
+
+fn print_name(name: &str) {
+    println!("name: {:?}", name);
+}
+
 fn main() {
 
     // vec macro, is similar to the code snipper below [vector manual]
@@ -71,5 +80,21 @@ fn main() {
     let emp = Employee {
         name: emp_name
     };
+
+    let receipt = vec![
+        LineItem{
+            name: "cereal".to_owned(),
+            count: 1,
+        },
+        LineItem{
+            name: String::from("fruit"),
+            count: 3,
+        }
+    ];
+
+    for item in receipt {
+        print_name(&item.name);
+        println!("count:{:?}", item.count);
+    }
 
 }
