a1dd17529fcd2374409b09414958959060fd9274
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-24 11:46:11 +0800

    Section 7 Part 54: Added more option statement examples

--- Section7/advanced_concepts/src/main.rs
+++ Section7/advanced_concepts/src/main.rs
@@ -95,6 +95,12 @@ fn find_quantity(name: &str) -> Option<i32> {
     None
 }
 
+struct Survey {
+    q1: Option<i32>,
+    q2: Option<bool>,
+    q3: Option<String>,
+}
+
 fn main() {
 
     let me = Employee {
@@ -188,4 +194,26 @@ fn main() {
     let quantity = find_quantity("bananas");
 
     println!("quantity: {:?}", quantity);
+
+    let response = Survey {
+        q1: Some(12),
+        q2: Some(true),
+        q3: Some("A".to_owned()),
+    };
+
+    match response.q1 {
+        Some(ans) => println!("q1: {:?}", ans),
+        None => println!("q1: no response")
+    }
+
+    match response.q2 {
+        Some(ans) => println!("q2: {:?}", ans),
+        None => println!("q2: no response")
+    }
+
+    match response.q3 {
+        Some(ans) => println!("q3: {:?}", ans),
+        None => println!("q3: no response")
+    }
+
 }
