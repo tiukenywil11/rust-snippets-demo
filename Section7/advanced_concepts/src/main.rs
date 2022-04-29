3baf61c794ecfa5d1149f70d2945b6eed32e7f93
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-29 12:37:35 +0800

    Section 7 Part 60: Added statement with result and question mark operator example

--- Section7/advanced_concepts/src/main.rs
+++ Section7/advanced_concepts/src/main.rs
@@ -123,6 +123,37 @@ fn get_sound(name: &str) -> Result<SoundData, String> {
 }
 */
 
+#[derive(Debug)]
+enum MenuChoice {
+    MainMenu,
+    Start,
+    Quit
+}
+
+fn get_choice(input: &str) -> Result<MenuChoice, String> {
+    match input {
+        "mainmenu" => Ok(MenuChoice::MainMenu),
+        "start" => Ok(MenuChoice::Start),
+        "quit" => Ok(MenuChoice::Quit),
+        _ => Err("menu choice not found".to_owned()),
+    }
+}
+
+fn print_choice(choice: &MenuChoice) {
+    println!("choice = {:?}", choice);
+}
+
+// question mark operator
+// returns nothing "()" unit type ", or error
+fn pick_choice(input: &str) -> Result<(), String> {
+    // question mark will automatically perform a match operation
+    // if successful, will take the inner data, else will return error
+    let choice = get_choice(input)?;
+    print_choice(&choice);
+    // return nothing
+    Ok(())
+}
+
 /// This is a documentation comment
 fn main() {
 
@@ -259,4 +290,21 @@ fn main() {
         Err(e) => println!("error: {:?}", e),
     }
     */
+
+    let choice = get_choice("leave");
+
+    //println!("choice = {:?}", choice);
+
+    /*
+    -- this will cause an error, because choice is a "Result" type, and print_choice needs a "MenuChoice" type
+    print_choice(&choice);
+    */
+
+    // this will fix print_choice function
+    match choice {
+        Ok(inner_choice) => print_choice(&inner_choice),
+        Err(e) => println!("error = {:?}", e),
+    }
+
+    pick_choice("start");
 }
